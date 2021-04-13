cdda26b7809a356a8b042a3691a26ccc3123beb7
Jiting <jiting@jtcat.com>
2021-04-14 06:14:31 +0800

    extract login prompt
    add devise zh-CN translation

---
+++ app/components/login_prompt_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class LoginPromptComponent < ViewComponent::Base
+
+end
---
+++ test/components/login_prompt_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class LoginPromptComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(LoginPromptComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
