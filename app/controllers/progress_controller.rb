d65e4e47f354c521b6db4f2b1e4d806f49d31f65
Jiting <jiting@jtcat.com>
2022-01-27 22:48:53 +0800

    update progress
    close #21 on gitlab

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -1,18 +1,31 @@
 class ProgressController < ApplicationController
   def total
-    total = -> { Entry.includes(:project_file)
-                      .references(:project_file)
-                      .where("(source != '(null)') and (source != '')")
-                      .group("project_files.type")
-                      .count }
-    have_contents = -> { Entry.includes(:project_file)
-                              .references(:project_file)
-                              .where("(source != '(null)') and (source != '') and ((chinese != '') or (entries.status = 4))")
-                              .group("project_files.type")
-                              .count }
+    total = lambda {
+      Entry.includes(:project_file)
+           .references(:project_file)
+           .where("(source != '(null)') and (source != '')")
+           .group("project_files.type")
+           .count
+    }
+    have_contents = lambda {
+      Entry.includes(:project_file)
+           .references(:project_file)
+           .where("(source != '(null)') and (source != '') and ((chinese != '') or (entries.status = 4))")
+           .group("project_files.type")
+           .count
+    }
+    proofreading = lambda {
+      Entry.includes(:project_file)
+           .references(:project_file)
+           .where("entries.status >= 1")
+           .group("project_files.type")
+           .count
+    }
 
     @total = total.call
     @have_contents = have_contents.call
+    @proofreading = proofreading.call
     @percentage = @total.map { |key, value| {key => ((@have_contents[key] || 0) / value.to_f * 100).round(3)} }.reduce(&:merge)
+    @proofreading_percentage = @total.map { |key, value| {key => ((@proofreading[key] || 0) / value.to_f * 100).round(3)} }.reduce(&:merge)
   end
 end
--- app/views/progress/total.json.jbuilder
+++ app/views/progress/total.json.jbuilder
@@ -3,6 +3,8 @@ json.status do
     json.type type
     json.total @total[type]
     json.have_contents @have_contents[type] || 0
+    json.proofreading @proofreading[type] || 0
     json.percentage @percentage[type]
+    json.proofreading_percentage @proofreading_percentage[type]
   end
 end
