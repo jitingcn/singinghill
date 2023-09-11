7b33ee955ae950747cea0dbd88aa8c3ffe527a89
Jiting <jiting@jtcat.com>
2023-09-11 18:04:02 +0800

    fix name

--- app/models/project_file.rb
+++ app/models/project_file.rb
@@ -1,13 +1,13 @@
 class ProjectFile < ApplicationRecord
   default_scope -> { order("id") }
-  enum status: { working: 0, done: 1 }
+  enum status: {working: 0, done: 1}
   has_many :entries, dependent: :destroy
 
   after_update_commit do
     broadcast_replace_to "main-app",
-                         target: "project_file_#{id}_progress",
-                         partial: "project_files/progress",
-                         locals: { project_file: instance_of?(ProjectFile) ? self : becomes(ProjectFile) }
+      target: "project_file_#{id}_progress",
+      partial: "project_files/progress",
+      locals: {project_file: instance_of?(ProjectFile) ? self : becomes(ProjectFile)}
   end
 
   include MeiliSearch::Rails
@@ -29,15 +29,15 @@ class ProjectFile < ApplicationRecord
     draft = 0 if draft.negative?
     proofreading = status.fetch("accept", 0) + status.fetch("double_check", 0) + status.fetch("final_check", 0)
     finished = status.fetch("finished", 0)
-    { filename: name, empty: empty, draft: draft, proofreading: proofreading, finished: finished }
+    {filename: name, empty: empty, draft: draft, proofreading: proofreading, finished: finished}
   end
 
   def page_num(options = {})
     column = options[:by] || :id
-    order  = options[:order] || :asc
-    per    = options[:per] || self.class.default_per_page
+    order = options[:order] || :asc
+    per = options[:per] || self.class.default_per_page
 
-    operator = (order == :asc ? "<=" : ">=")
+    operator = ((order == :asc) ? "<=" : ">=")
     (self.class
          .where("#{column} #{operator} ?", read_attribute(column))
          .order("#{column} #{order}").count.to_f / per).ceil
@@ -50,18 +50,18 @@ class ProjectFile < ApplicationRecord
   end
 
   class << self
-    def download_all(source=false)
-      dir = Dir.mktmpdir("#{self.class.name.demodulize}")
+    def download_all(source = false)
+      dir = Dir.mktmpdir("ProjectFile_")
       begin
         all.order(:id).each do |project_file|
           next if project_file.entries.order(:index).where.not(chinese: "").count.zero?
-  
+
           File.open("#{dir}/#{project_file.name}", "w", encoding: "UTF-8") do |file|
             file.write(project_file.to_txt(source: source))
           end
         end
         file_list = `ls -1d #{dir}/*`.split.join(" ")
-        file = "#{Rails.root}/public/tmp/#{self.class.name.demodulize}_#{source ? '_source_': ''}#{Time.now.to_i}.zip"
+        file = "#{Rails.root}/public/tmp/ProjectFile_#{source ? "_source_" : ""}#{Time.now.to_i}.zip"
         `zip -9 -j #{file} #{file_list}`
       ensure
         RemoveTmpDirJob.perform_later(dir)
