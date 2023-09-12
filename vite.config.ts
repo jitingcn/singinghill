f6511cac3d7ae41887e723c0fd4aa52403cc6f85
jiting <jiting@jtcat.com>
2023-09-12 14:05:22 +0800

    remove compression

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
 import legacy from '@vitejs/plugin-legacy'
-import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
@@ -11,11 +10,5 @@ export default defineConfig({
     FullReload(['config/routes.rb', 'app/views/**/*', 'app/components/**/*'], { delay: 500 }),
     StimulusHMR(),
     legacy(),
-    viteCompression({ algorithm: 'gzip', ext: '.gz' }),
-    viteCompression({ algorithm: 'brotliCompress', ext: '.br' })
   ],
-  build: {
-    target: "es2017",
-    minify: "terser"
-  }
 })
